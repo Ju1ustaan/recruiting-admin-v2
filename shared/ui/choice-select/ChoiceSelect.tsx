dd5f47d7f3dcfcd8b2a05b343e1bfabafb290670
luijulus <luijulus@gmail.com>
2026-03-25 21:56:13 +0500

    fix types in ChoiceSelect

--- shared/ui/choice-select/ChoiceSelect.tsx
+++ shared/ui/choice-select/ChoiceSelect.tsx
@@ -1,21 +1,26 @@
 import { Picture } from "@/entities/picture"
-import { Ecosystem } from "@/entities/vacancy-ecosystem"
+
+interface FormState {
+    name: string
+    description: string
+    picture: string
+}
 
 export interface SelectProps {
-    selected: Ecosystem
+    selected: FormState
     items: Picture[]
     choiceImg: string
     setChoiceImg: (value: string) => void
 }
 
-export const ChoiceSelect = ({ selected, items, setChoiceImg, choiceImg }: SelectProps) => {
+export const ChoiceSelect = ({ items, setChoiceImg, choiceImg }: SelectProps) => {
     return (
         <select
             value={choiceImg}
             onChange={(e) => setChoiceImg(e.target.value)}
             className="w-full p-1.5 text-sm rounded-lg border border-gray-300 outline-blue-400"
-            name={selected.ecosystemName}
-            id={`ecosystem-${selected.ecosystemName}`}
+            name={choiceImg}
+            id={`ecosystem-${choiceImg}`}
         >
             <option value="">Выберите картинку</option>
             {items.map((item) => (
