4b0b62dd4b448db3ef87e7315acad0d49eb2dc43
luijulus <luijulus@gmail.com>
2026-03-25 09:10:16 +0500

    localStorage fix

--- widgets/admin-profile/model/useProfile.ts
+++ widgets/admin-profile/model/useProfile.ts
@@ -1,28 +1,33 @@
 'use client'
 
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 const BACKGROUNDS = [
-    '/blue-background-1.jpg',
-    '/yellow-background.jpg',
-    '/purple-background.jpg',
-    '/rainbow-sky-background.jpg'
+  '/blue-background-1.jpg',
+  '/yellow-background.jpg',
+  '/purple-background.jpg',
+  '/rainbow-sky-background.jpg'
 ]
 
 export const useProfile = () => {
-    const [selectedBackground, setSelectedBackground] = useState(
-        () => localStorage.getItem('admin-background') ?? BACKGROUNDS[0]
-    )
+  const [selectedBackground, setSelectedBackground] = useState(BACKGROUNDS[0])
 
-    const changeBackground = (url: string) => {
-        setSelectedBackground(url)
-        localStorage.setItem('admin-background', url)
-        window.dispatchEvent(new Event('storage'))
+  useEffect(() => {
+    const stored = localStorage.getItem('admin-background')
+    if (stored) {
+      setSelectedBackground(stored)
     }
+  }, [])
 
-    return {
-        backgrounds: BACKGROUNDS,
-        selectedBackground,
-        changeBackground,
-    }
+  const changeBackground = (url: string) => {
+    setSelectedBackground(url)
+    localStorage.setItem('admin-background', url)
+    window.dispatchEvent(new Event('storage'))
+  }
+
+  return {
+    backgrounds: BACKGROUNDS,
+    selectedBackground,
+    changeBackground,
+  }
 }
